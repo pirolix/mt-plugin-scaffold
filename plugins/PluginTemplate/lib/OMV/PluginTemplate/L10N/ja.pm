aeaa15140c866e1da397809eb5a2e1c169e08900
pirolix <pirolix@adbff654-c4ea-448a-8ae3-cad037306009>
2013-07-20 07:49:12 +0000

    * 編集しやすく
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/__scaffold/trunk@343 adbff654-c4ea-448a-8ae3-cad037306009

--- plugins/PluginTemplate/lib/OMV/PluginTemplate/L10N/ja.pm
+++ plugins/PluginTemplate/lib/OMV/PluginTemplate/L10N/ja.pm
@@ -6,11 +6,22 @@ use base 'OMV::PluginTemplate::L10N::en_us';
 use vars qw( %Lexicon );
 
 %Lexicon = (
+    # *.pl
     'Display the nickname and username' => '管理画面にユーザ名とニックネームを表示します',
-    'Display Format' => '表示形式',
-    'Username (Nickname)' => 'ユーザ名 (表示名)',
-    'Nickname (Username)' => '表示名 (ユーザ名)',
-    'Nickname' => '表示名のみ',
+    # Callbacks.pm
+    '' => '',
+    # Methods.pm
+    '' => '',
+    # Tags.pm
+    '' => '',
+    # config.tmpl
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
 );
 
 1;
