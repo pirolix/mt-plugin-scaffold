c846376846903ba2bbc1458bf314e74bafd33c31
pirolix <pirolix@adbff654-c4ea-448a-8ae3-cad037306009>
2013-07-29 06:25:11 +0000

    * フォルダが存在しない時の分岐
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/__scaffold/trunk@352 adbff654-c4ea-448a-8ae3-cad037306009

--- plugins/PluginTemplate/.lint.sh
+++ plugins/PluginTemplate/.lint.sh
@@ -23,19 +23,19 @@ find ./ -type f -name '*.pm' \
 		-I$PWD/lib \
 		{} \;
 
-echo "* Checking MT Templates ..."
-find ./tmpl -type f -name '*.tmpl' \
-	-exec perl \
-		-I$MT_HOME/extlib \
-		-I$MT_HOME/lib \
-		-I$PWD/extlib \
-		-I$PWD/lib \
-		-MMT \
-		-MMT::Template \
-		-e '$m = MT->new or die MT->errstr;' \
-		-e '($f = $ARGV[1]) =~ s/^\./$ARGV[0]/;'\
-		-e '$t = MT::Template->new_file($f);' \
-		-e 'print $t->build ? "$ARGV[1] syntax OK\n" : $t->errstr;' \
-		$PWD {} \;
-
-echo
+if [ -d ./tmpl ]; then
+	echo "* Checking MT Templates ..."
+	find ./tmpl -type f -name '*.tmpl' \
+		-exec perl \
+			-I$MT_HOME/extlib \
+			-I$MT_HOME/lib \
+			-I$PWD/extlib \
+			-I$PWD/lib \
+			-MMT \
+			-MMT::Template \
+			-e '$m = MT->new or die MT->errstr;' \
+			-e '($f = $ARGV[1]) =~ s/^\./$ARGV[0]/;'\
+			-e '$t = MT::Template->new_file($f);' \
+			-e 'print $t->build ? "$ARGV[1] syntax OK\n" : $t->errstr;' \
+			$PWD {} \;
+fi
